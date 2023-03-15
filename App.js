8db5358dfc78b4c26777358dd08466d0a540042b
Gaby <89497550+GabbTt@users.noreply.github.com>
2023-03-15 17:11:52 -0500

    PantallaLogin

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from './screens/login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Login />
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
